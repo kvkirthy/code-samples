9339ff72347c680ec6ca1232e88a70b35aba7763
Keerti Kotaru <kvkirthy@gmail.com>
2022-09-12 18:39:13 +0530

    Breakpoint observer integrated

--- src/app/breakpoint-sample/adapt-by-screensize/adapt-by-screensize.component.ts
+++ src/app/breakpoint-sample/adapt-by-screensize/adapt-by-screensize.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adapt-by-screensize.component.scss']
 })
 export class AdaptByScreensizeComponent implements OnInit {
+  message = "";
+  isPortrait = false;
 
-  constructor() { }
+  constructor(private bpObserable: BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.bpObserable
+      .observe([
+        '(orientation: portrait)',
+      ])
+      .subscribe(result => {
+        if(result.matches){
+          this.isPortrait = true;
+          this.message = 'Turn phone to landscape to see larger image';
+        }
+      });
+
+      this.bpObserable
+      .observe([
+        '(orientation: landscape)',
+      ])
+      .subscribe(result => {
+        if(result.matches){
+          this.isPortrait = false;
+          this.message = 'Turn phone to portrait to see museum timings';
+        }
+      });
   }
 
 }
